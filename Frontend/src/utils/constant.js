1d1b605e35dd9b6ce9925007868349f106880663
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2025-01-15 07:55:27 +0530

    v2

---
+++ Frontend/src/utils/constant.js
@@ -0,0 +1,30 @@
+// export const USER_API_END_POINT =
+//   "https://job-borad-3ose.onrender.com/api/v1/user";
+// export const JOB_API_END_POINT =
+//   "https://job-borad-3ose.onrender.com/api/v1/job";
+// export const APPLICATION_API_END_POINT =
+//   "https://job-borad-3ose.onrender.com/api/v1/application";
+// export const COMPANY_API_END_POINT =
+//   "https://job-borad-3ose.onrender.com/api/v1/company";
+
+// export const USER_API_END_POINT = "http://206.189.138.106:8080/api/v1/user";
+// export const JOB_API_END_POINT = "http://206.189.138.106:8080/api/v1/job";
+// export const APPLICATION_API_END_POINT =
+//   "http://206.189.138.106:8080/api/v1/application";
+// export const COMPANY_API_END_POINT =
+//   "http://206.189.138.106:8080/api/v1/company";
+
+export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
+export const APPLICATION_API_END_POINT =
+  "http://localhost:8080/api/v1/application";
+export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
+
+// export const USER_API_END_POINT =
+//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/user";
+// export const JOB_API_END_POINT =
+//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/job";
+// export const APPLICATION_API_END_POINT =
+//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/application";
+// export const COMPANY_API_END_POINT =
+//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/company";
