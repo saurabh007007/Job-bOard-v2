5c6876e95a0f8d5fc713e0d5c1008139206ea132
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2025-01-15 08:23:08 +0530

    add

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://job-b-oard-v2-pufhscl2l-saurabh007007s-projects.vercel.app",
+  ],
   credentials: true,
 };
 
