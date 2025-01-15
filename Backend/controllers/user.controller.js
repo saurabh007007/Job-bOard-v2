83b274ea1ee57b9ed99e85fb10216173330a132a
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2025-01-15 08:37:12 +0530

    add

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -99,7 +99,10 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
+
+        secure: process.env.NODE_ENV === "production",
+
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       })
       .Header("Access-Control-Allow-Origin", "*", "token", token)
       .json({
