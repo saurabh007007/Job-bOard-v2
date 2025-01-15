0a982de99689727e5112f3e4508df26e5d900c30
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2025-01-15 08:19:20 +0530

    addd

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,30 +1,14 @@
-// export const USER_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/user";
-// export const JOB_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/job";
+// export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+// export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
 // export const APPLICATION_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/application";
-// export const COMPANY_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/company";
+//   "http://localhost:8080/api/v1/application";
+// export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
 
-// export const USER_API_END_POINT = "http://206.189.138.106:8080/api/v1/user";
-// export const JOB_API_END_POINT = "http://206.189.138.106:8080/api/v1/job";
-// export const APPLICATION_API_END_POINT =
-//   "http://206.189.138.106:8080/api/v1/application";
-// export const COMPANY_API_END_POINT =
-//   "http://206.189.138.106:8080/api/v1/company";
-
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
+export const USER_API_END_POINT =
+  "https://job-board-v2-production.up.railway.app/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-board-v2-production.up.railway.app/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8080/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
-
-// export const USER_API_END_POINT =
-//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/user";
-// export const JOB_API_END_POINT =
-//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/job";
-// export const APPLICATION_API_END_POINT =
-//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/application";
-// export const COMPANY_API_END_POINT =
-//   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/company";
+  "https://job-board-v2-production.up.railway.app/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-board-v2-production.up.railway.app/api/v1/company";
